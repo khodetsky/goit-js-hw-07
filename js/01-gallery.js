1237610b05f302dba2e6038e7f386806430af0a9
khodetsky <hodyaya@gmail.com>
2022-05-23 00:52:55 +0300

    done task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,46 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+const imagesMarcup = createImagesCardsMarcup(galleryItems);
+
+gallery.insertAdjacentHTML('beforeend', imagesMarcup)
+
+function createImagesCardsMarcup(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+            return `
+            <div class="gallery__item">
+              <a class="gallery__link" href="${original}">
+                <img
+                  class="gallery__image"
+                  src="${preview}"
+                  data-source="${original}"
+                  alt="${description}"
+                />
+              </a>
+            </div>
+            `
+        })
+        .join('');
+};
+
+gallery.addEventListener('click', blockLinkOnImageClic);
+gallery.addEventListener('click', openModalImage);
+
+function blockLinkOnImageClic(evt) {
+    evt.preventDefault();
+};
+
+function openModalImage(evt) {
+    const clicOnImage = evt.target.classList.contains('gallery__image');
+    const modalImage = basicLightbox.create(`
+    <img src="${evt.target.dataset.source}" width="1280" height="800">
+    `).show();
+    if (!clicOnImage) {
+        return;
+    }
+    return modalImage;
+}
+
